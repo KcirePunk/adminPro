d4239a345c70c753eb3bc04e0d2d32124b94c1af
KcirePunk <kcire_792_efta@hotmail.com>
2019-08-20 12:38:20 -0500

    Primer Commit

---
+++ src/app/pages/gradicas1/gradicas1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gradicas1',
+  templateUrl: './gradicas1.component.html',
+  styles: []
+})
+export class Gradicas1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
